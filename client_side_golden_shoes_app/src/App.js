c245683fd725bbba14dbc5979b695c350cb4f749
sara <smhumphries@hotmail.co.uk>
2021-09-10 17:01:21 +0100

    adds nav bar and top bar

--- client_side_golden_shoes_app/src/App.js
+++ client_side_golden_shoes_app/src/App.js
@@ -2,19 +2,20 @@
 import './App.css';
 import StockContainer from './containers/StockContainer';
 import HomePage from './components/HomePage';
+import TopBar from './components/TopBar';
 import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div id="whole-app">
+        <TopBar></TopBar>
         <NavBar></NavBar>
         <Switch>
           <Route path = '/' exact component={HomePage}></Route>
           <Route path = '/stock' exact component={StockContainer}></Route>
         </Switch>
-        
       </div>
     </Router>
   );
---
+++ client_side_golden_shoes_app/src/components/TopBar.js
@@ -0,0 +1,12 @@
+
+const TopBar = function() {
+    return (
+        <nav id="nav-bar">
+                <a href='/'>FAQ</a>
+                <a href='/'>Live Chat</a>
+                <a href='/'>Track Orders</a>     
+        </nav>
+    );
+};
+
+export default TopBar;
