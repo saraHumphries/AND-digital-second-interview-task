8f28ea066e2e93e739d4ed5cd033c17367998b24
sara <smhumphries@hotmail.co.uk>
2021-09-11 17:08:04 +0100

    adds update sold status route for stock items

--- server_side_golden_shoes_app/src/main/java/com/golden_shoes/golden_shoes_app/controllers/StockItemController.java
+++ server_side_golden_shoes_app/src/main/java/com/golden_shoes/golden_shoes_app/controllers/StockItemController.java
@@ -5,12 +5,10 @@ import com.golden_shoes.golden_shoes_app.repositories.StockItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StockItemController {
@@ -26,4 +24,23 @@ public class StockItemController {
         }
         return new ResponseEntity<>(stockItemRepository.findStockItemsByShoeTypeId(shoeTypeId), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/stock_items/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Optional<StockItem>> getStockItemById(@PathVariable Long id) {
+        return new ResponseEntity<>(stockItemRepository.findById(id), HttpStatus.OK);
+    }
+
+
+
+    @PutMapping(value = "/stock_items/{stockItemId}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<StockItem> updateStockItemSoldStatus(@RequestBody StockItem stockItem, @PathVariable Long stockItemId) {
+        StockItem stockItemToUpdate = stockItemRepository.findById(stockItemId).get();
+        stockItemToUpdate.setSoldStatus(stockItem.getSoldStatus());
+        final StockItem updatedStockItem = stockItemRepository.save(stockItemToUpdate);
+        return new ResponseEntity<>(updatedStockItem, HttpStatus.OK);
+
+    }
+
 }
