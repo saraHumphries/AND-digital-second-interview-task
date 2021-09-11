305700685a01458951f6ce0164b2183fcc4fae2b
sara <smhumphries@hotmail.co.uk>
2021-09-11 14:24:43 +0100

    adds state for available sizes and colours

--- client_side_golden_shoes_app/src/components/ShowShoe.js
+++ client_side_golden_shoes_app/src/components/ShowShoe.js
@@ -1,10 +1,33 @@
 import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
 
 const ShowShoe = function() {
 
     const data = useLocation();
     const shoeType = data.state.shoeType;
     const stockInventory = data.state.stockInventory;
+
+    const [availableColours, setAvailableColours] = useState([]);
+    const [availableSizes, setAvailableSizes] = useState([]);
+    const [selectedColor, setSelectedColour] = useState(null);
+    const [selectedSize, setSelectedSize] = useState(null);
+    
+    useEffect(() => {
+        const availableColours = [];
+        for (const stockItem of stockInventory) {
+            if (!availableColours.includes(stockItem.colour)) {
+                availableColours.push(stockItem.colour);
+            }
+        }
+        setAvailableColours(availableColours);
+        const availableSizes = [];
+        for (const stockItem of stockInventory) {
+            if (!availableSizes.includes(stockItem.size)) {
+                availableSizes.push(stockItem.size);
+            }
+        }
+        setAvailableSizes(availableSizes.sort());
+    }, []);
 
     console.log(shoeType);
 
@@ -15,6 +38,13 @@ const ShowShoe = function() {
                 <h3 className='brand-name shoe-text'>{shoeType.brand}</h3>
                 <h5 className='shoe-description shoe-text'>{shoeType.description}</h5>
                 <h5 className='price shoe-text'>£{shoeType.price}</h5>
+            </div>
+
+            <div className='size-color-form-container'>
+                <form className='size-color-form'>
+                    <label for='size'>Choose a size</label>
+                        
+                </form>
             </div>
         </div>
     );
