42308fd130367ac185e112459cd5ed4fec9eec88
sara <smhumphries@hotmail.co.uk>
2021-09-12 11:47:38 +0100

    adds dispatch and delivery info to orders page

--- client_side_golden_shoes_app/src/components/Order.js
+++ client_side_golden_shoes_app/src/components/Order.js
@@ -6,16 +6,32 @@ const Order = function({order}) {
         return <OrderItem orderItem = {orderItem} key={orderItem.id}></OrderItem>
     });
 
-    
 
+
+    const getStatuses = function() {
+        const statuses = {};
+        if (order.dispatchedStatus) {
+            statuses.dispatchedText = 'THIS ORDER HAS BEEN DISPATCHED'
+        } else {
+            statuses.dispatchedText = 'NOT YET DISPATCHED'
+        }
+        if (order.deliveredStatus) {
+            statuses.deliveredText = 'AND SUCCESSFULLY DELIVERED'
+        } else {
+            statuses.deliveredText = 'NOT YET DELIVERED'
+        }
+        return statuses;
+    };
+
+    console.log(getStatuses());
 
     return (
         <div className='whole-order'>
-            <h5>ORDER PLACED ON {order.orderDate} FOR £{order.totalCost}</h5>
+            <h5>ORDER {order.id} -- PLACED ON {order.orderDate} FOR £{order.totalCost}</h5>
             <p>DELIVERY ADDRESS: {order.customer.address}</p>
-            <p>DISPATCHED: {order.dispatchedStatus}</p>
-            <p>ORDER CONTAINS</p>
             {orderItems}
+            <p>{getStatuses().dispatchedText}</p>
+            <p>{getStatuses().deliveredText}</p>
             
             
         </div>
