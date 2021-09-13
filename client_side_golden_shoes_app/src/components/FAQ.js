9d5bf2803032ba841fbedfebf2268876ff0546af
sara <smhumphries@hotmail.co.uk>
2021-09-13 12:44:45 +0100

    adds accordion menu to FAQ page

--- client_side_golden_shoes_app/src/components/FAQ.js
+++ client_side_golden_shoes_app/src/components/FAQ.js
@@ -1,8 +1,45 @@
 
+import './AccordionMenu.css';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
 const FAQ = function() {
+
+    const questionsAndAnswers = [
+        {
+            id: 1,
+            heading: 'What?',
+            content: 'yes'
+        },
+        {
+            id:2,
+            heading: 'Why?',
+            content: 'no'
+        }
+    ];
+
     return (
         <div className='main-container'>
             <h1>FAQs</h1>
+            <Accordion>
+                {questionsAndAnswers.map((question) => {
+                    return <AccordionItem key={question.id}>
+                        <AccordionItemHeading className='accordion-question'>
+                            <AccordionItemButton>
+                                {question.heading}
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel className='accordion-answer'>
+                            {question.content}
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                })}
+            </Accordion>
         </div>
     );
 };
