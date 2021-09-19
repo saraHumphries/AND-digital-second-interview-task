c0dfe578754aba4e64753cb7864d86b12c3cde87
sara <smhumphries@hotmail.co.uk>
2021-09-19 13:57:25 +0100

    shows when shoes are out of stock

--- client_side_golden_shoes_app/src/components/ShoeType.js
+++ client_side_golden_shoes_app/src/components/ShoeType.js
@@ -1,23 +1,33 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
+import StockService from "../services/StockService";
+import { useEffect, useState } from 'react';
 
 const ShoeType = function({category, shoeType}) {
 
+    const [stockLevels, setStockLevels] = useState([]);
+
+    useEffect(() => {
+        StockService.getUnsoldStockItemsByShoeTypeId(shoeType.id)
+            .then(res => setStockLevels(res));
+    }, []);
 
     return (
         <div>
-            <Link to={{
+            {stockLevels.length == 0? <img className="image-icon out-of-stock-shoe-image" src={shoeType.imageUrl}></img>
+            : <Link to={{
                 pathname: `/stock/${category}/${shoeType.brand}/${shoeType.description}`,
                 state: {shoeType}
                 }}>
                 <div className='shoe-display'>
                     <img className="image-icon"src={shoeType.imageUrl}></img>
                 </div>
-            </Link>
+            </Link>}
                 <div className='shoe-advert-text'>
                     <h3 className='brand-name shoe-text'>{shoeType.brand}</h3>
                     <h5 className='shoe-description shoe-text'>{shoeType.description}</h5>
                     <h5 className='price shoe-text'>£{shoeType.price}</h5>
+                    {stockLevels.length == 0? <h5 className='out-of-stock-text'>OUT OF STOCK</h5> : null}
                 </div>
         </div>
             
