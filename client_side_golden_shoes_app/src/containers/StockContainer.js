5bd7195f1b87e1948339a306fc6ec463714352e4
sara <smhumphries@hotmail.co.uk>
2021-09-21 12:44:52 +0100

    adds (non-functional) filters for shoes)

--- client_side_golden_shoes_app/src/containers/StockContainer.js
+++ client_side_golden_shoes_app/src/containers/StockContainer.js
@@ -25,6 +25,23 @@ const StockContainer = function() {
 
     return (
         <div className='main-container'>
+            <section className="filter-section">
+                <div className='filter'>
+                    <label htmlFor='brand'>FILTER BY BRAND</label>
+                    <select id='brand'>
+                        <option>ADIDAS</option>
+                        <option>CONVERSE</option>
+                    </select>
+                </div>
+
+                <div className='filter'>
+                    <label htmlFor='price'>FILTER BY PRICE</label>
+                    <select id='price'>
+                        <option>UNDER £50</option>
+                        <option>£50 - £100</option>
+                    </select>
+                </div>
+            </section>
             <div id='shoe-type-list'>
             {listOfShoeTypes}
             </div>
